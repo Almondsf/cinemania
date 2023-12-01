624c749bb7cd0743f696d90b48bdf190b29bb06e
Oluwafemi Oloyede <oloyedeoluwafemi22@gmail.com>
2023-12-01 12:03:39 +0100

    some stylings

---
+++ components/nav/navbar.js
@@ -0,0 +1,78 @@
+import styles from "./navbar.module.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useState } from "react";
+import Image from "next/image";
+
+const Navbar = (props) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const router = useRouter();
+  const { username } = props;
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+  const handleLogout = () => {
+    e.preventDefault();
+  };
+  const handleShowDropdown = (e) => {
+    e.preventDefault();
+    setShowDropdown(!showDropdown);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>
+            <Image
+              src="/static/netflix.svg"
+              alt="Netflix logo"
+              width="111"
+              height="30"
+            />
+          </div>
+        </a>
+
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <button className={styles.usernameBtn} onClick={handleShowDropdown}>
+            <p className={styles.username}>{username}</p>
+          </button>
+          {/* expand more */}
+          <Image
+            src="/static/expand_more.svg"
+            alt="Expand dropdown"
+            width="24"
+            height="24"
+          />
+          {showDropdown && (
+            <div className={styles.navDropdown}>
+              <Link
+                href="/login"
+                onClick={handleLogout}
+                className={styles.linkName}
+              >
+                Sign out
+              </Link>
+              <div className={styles.lineWrapper}></div>
+            </div>
+          )}
+        </nav>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
